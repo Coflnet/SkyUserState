15e5dcad5f7c3eade4e7cbc88d3c54cc8b5bfec8
Äkwav <16632490+Ekwav@users.noreply.github.com>
2022-08-07 12:46:48 +0200

    Initial commit

---
+++ Models/Flip.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Coflnet.Sky.Base.Models
+{
+    [DataContract]
+    public class Flip
+    {
+        [IgnoreDataMember]
+        [JsonIgnore]
+        public int Id { get; set; }
+        public int AuctionId { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Timestamp]
+        [DataMember(Name = "timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ Services/BaseService.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Coflnet.Sky.Base.Models;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coflnet.Sky.Base.Services
+{
+    public class BaseService
+    {
+        private BaseDbContext db;
+
+        public BaseService(BaseDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Flip> AddFlip(Flip flip)
+        {
+            if (flip.Timestamp == default)
+            {
+                flip.Timestamp = DateTime.Now;
+            }
+            db.Flips.Add(flip);
+            await db.SaveChangesAsync();
+            return flip;
+        }
+    }
+}
