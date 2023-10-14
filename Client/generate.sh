e5ba0255ec171a2a0068e5550f38aa93a7536504
Äkwav <ekwav@coflnet.com>
2023-10-14 17:53:15 +0200

    update client

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,8 +1,8 @@
-VERSION=0.1.0
+VERSION=0.1.1
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5025/swagger/v1/swagger.json \
--g csharp-netcore \
+-g csharp \
 -o /local/out --additional-properties=packageName=Coflnet.Sky.PlayerState.Client,packageVersion=$VERSION,licenseId=MIT
 
 cd out
