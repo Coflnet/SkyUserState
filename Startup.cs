633b89ba2a3d2a3b042b641d3c6df0b021ece262
Äkwav <ekwav@coflnet.com>
2023-11-23 18:49:11 +0100

    add errohandler middleware

--- Startup.cs
+++ Startup.cs
@@ -83,6 +83,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        ErrorHandler.Add(app, "playerstate");
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
