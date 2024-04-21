68f733bdce17e6edbb99d1eb77be452d44965bb9
Äkwav <ekwav@coflnet.com>
2024-04-21 13:11:54 +0200

    log all recipes

--- Services/Listener/RecipeUpdate.cs
+++ Services/Listener/RecipeUpdate.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -8,8 +9,8 @@ public class RecipeUpdate : UpdateListener
     /// <inheritdoc/>
     public override async Task Process(UpdateArgs args)
     {
-        if (!(args.msg.Chest?.Name.EndsWith("Recipes") ?? false))
+        if (!(args.msg.Chest?.Name.Contains("Recipe") ?? false))
             return;
-        Console.WriteLine($"Recipe update {args.msg.Chest?.Name} {JsonConvert.SerializeObject(args.msg.Chest?.Items)}");
+        Console.WriteLine($"Recipe update {args.msg.Chest?.Name} {JsonConvert.SerializeObject(args.msg.Chest?.Items.Take(36))}");
     }
 }
