10c39eb73cc6c9ce96af390a08fda44cc558e680
Äkwav <ekwav@coflnet.com>
2024-06-11 00:21:58 +0200

    try to batch again

--- Services/MigrationService.cs
+++ Services/MigrationService.cs
@@ -48,7 +48,7 @@ public class MigrationService : BackgroundService
         {
             logger.LogInformation($"Migrating {tag} at {cacheKey}");
             var items = await oldTable.Where(t => t.Tag == tag).ExecuteAsync();
-            foreach (var item in Batch(items, 1))
+            foreach (var item in Batch(items, 5))
             {
                 _ = Task.Run(async () =>
                 {
