114bbfedfd9de341ab1c0d234706144e0f43e869
Äkwav <ekwav@coflnet.com>
2024-08-22 16:49:37 +0200

    use core parsing

--- Services/Listener/CoinParser.cs
+++ Services/Listener/CoinParser.cs
@@ -20,25 +20,14 @@ public class CoinParser
 
     private static long ParseCoinAmount(string stringAmount)
     {
-        var parsed = 0d;
-        if (stringAmount.EndsWith("B"))
-            parsed = double.Parse(stringAmount.Trim('B'), CultureInfo.InvariantCulture) * 1_000_000_000;
-        else if (stringAmount.EndsWith("M"))
-            parsed = double.Parse(stringAmount.Trim('M'), CultureInfo.InvariantCulture) * 1_000_000;
-        else if (stringAmount.EndsWith("k"))
-            parsed = double.Parse(stringAmount.Trim('k'), CultureInfo.InvariantCulture) * 1_000;
-        else
-            parsed = double.Parse(stringAmount, CultureInfo.InvariantCulture);
-
-        return (long)(parsed * 10);
+        return Core.CoinParser.ParseCoinAmount(stringAmount);
     }
 
     public long GetInventoryCoinSum(IEnumerable<Item> items)
     {
-        var withSumary = items.Where(i => i.Description?.Contains("Total Coins Offered:") ?? false).FirstOrDefault();
-        if (withSumary != null)
+        if(Core.CoinParser.TryParseFromDescription(items.Select(i => i.Description), out var result))
         {
-            return ParseCoinAmount(withSumary.Description!.Substring(withSumary.Description.IndexOf("Total Coins Offered:") + 2));
+            return result;
         }
         return items.Sum(GetCoinAmount);
     }
